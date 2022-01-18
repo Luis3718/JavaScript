a4a5b0025ec9421b569fc1c77e013393a6b035e2
Luis Manuel <Alvarado3718@hotmail.com>
2022-01-17 18:53:22 -0600

    Aprendi sobre variables y tipos de datos

---
+++ valores.js
@@ -0,0 +1,13 @@
+/*Tipo de Valores primitivos*/
+
+40 /*Tipo de valor entero*/
+"Luis Manuel Alvarado Romero" /*Tipo de valor string*/
+true /*Tipo de valor booleano*/
+false /*Tipo de valor booleano*/
+null /*Tipo de valor place-holder o valores vacios*/
+undefined /*Tipo de valor place-holder o valores vacios, tener cuidado con undefined*/
+
+/*Tipos de Valores no primitivos o valores tipo objetos*/
+
+[1,5,25] /*Tipo de valor objeto array*/
+{nombre: "Luis"} /*Tipo de valor objeto*/
