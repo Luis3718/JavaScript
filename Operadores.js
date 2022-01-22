40e9fd8b15ce9d1ff0f99c81add64034f0eb3c32
Luis Manuel <Alvarado3718@hotmail.com>
2022-01-21 21:54:53 -0600

    Aprendi acerca de los distintos operadores que usa javascript

---
+++ Operadores.js
@@ -0,0 +1,23 @@
+/*Tipos de operadores*/
+//operadores de asignacion
+var a=3; //el operador = es un operador de asignacion
+
+//operadores de comparacion
+3=="3"; //compara si el valor es igual al de la derecha
+3==="3" //compara si el valor y el tipo de dato es igual al de la derecha
+5>3 //compara si el de la izquierda es mayor que el de la derecha
+5<3 //compara si el de la izquierda es menor que el de la derecha
+5>=3 //compara si el de la izquierda es mayor o igual que el de la derecha
+5<=3 //compara si el de la izquierda es menor o igual que el de la derecha
+a&&b //Crea una sentencia de cortocircuito para que se complan las 2
+a||b //Crea una sentencia de cortocircuito para ver si alguna es verdad
+
+//operadores aritmeticos
+3+2
+50-10
+10*20
+20/2
+
+"Luis" + "Manuel" //concatenamos un string
+
+!false //negacion u operadores unitarios
